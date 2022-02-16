af80dbe6d15cc1a75613e8790398e259298e6daf
Moravec4, Lukas <lukas.moravec4@firma.seznam.cz>
2022-02-16 15:34:24 +0100

    loops instead of recursion on nodes

--- cHashTableNode.h
+++ cHashTableNode.h
@@ -40,37 +40,47 @@ cHashTableNode<TKey, TData>::~cHashTableNode()
 template<class TKey, class TData>
 bool cHashTableNode<TKey, TData>::Add(const TKey &key, const TData &data, cMemory *memory, int &itemCount, int &nodeCount)
 {
-	bool ret = true;
+    // if is empty node, simply add the key and data
+    // if is not empty, check key, if mKey and key are same, return false
+    // if nextNode is nullptr, allocate and insert the data
+    auto nextNodeInChain = this;
+    while (!nextNodeInChain->mEmptyNode) {
+        if (nextNodeInChain->mKey == key) {
+            return false;
+        }
+        if (nextNodeInChain->mNextNode == nullptr) {
+            if (memory == nullptr) {
+                nextNodeInChain->mNextNode = new cHashTableNode<TKey, TData>();
+            } else {
+                auto mem = memory->New(sizeof (cHashTableNode<TKey, TData>));
+                nextNodeInChain->mNextNode = new (mem)cHashTableNode<TKey, TData>();
+            }
+            nodeCount++;
+            nextNodeInChain = nextNodeInChain->mNextNode;
+            break;
+        }
+        nextNodeInChain = nextNodeInChain->mNextNode;
+    }
 
-	if (!mEmptyNode) {
-		if (mKey == key) {
-			ret = false;
-		}
-		else {
-			if (mNextNode == nullptr) {
-                if (memory == nullptr) {
-                    mNextNode = new cHashTableNode<TKey, TData>();
-                } else {
-                    auto mem = memory->New(sizeof (cHashTableNode<TKey, TData>));
-                    mNextNode = new (mem)cHashTableNode<TKey, TData>();
-                }
-				nodeCount++;
-			}
-			ret = mNextNode->Add(key, data, memory, itemCount, nodeCount);
-		}
-	} else {
-		mKey = key;
-		mData = data;
-		mEmptyNode = false;
-		itemCount++;
-		ret = true;
-	}
-	return ret;
+    nextNodeInChain->mKey = key;
+    nextNodeInChain->mData = data;
+    nextNodeInChain->mEmptyNode = false;
+    itemCount++;
+    return true;
 }
 
 template<class TKey, class TData>
 bool cHashTableNode<TKey, TData>::Find(const TKey &key, TData &data) const
 {
+    auto nextNodeInChain = this;
+    while (nextNodeInChain->mKey != key) {
+        if (nextNodeInChain->mNextNode == nullptr) {
+            return false;
+        }
+        nextNodeInChain = nextNodeInChain->mNextNode;
+    }
+    data = nextNodeInChain->mData;
+    return true;
 	if (mKey == key) {
 		data = mData;
 		return true;
